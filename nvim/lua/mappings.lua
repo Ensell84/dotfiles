61a394876d926caeaf912ab7ae22c781251fafc3
Egor Bondar <a23134522@icloud.com>
2025-03-22 17:56:27 +0700

    Neovim configs

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,14 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+-- when Highlighted in Visual -> J/K to move up/down selected lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
